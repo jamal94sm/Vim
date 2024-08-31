7eb16bfa709a61fdf1afaf51b10d85618b594db7
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2024-08-31 19:07:24 +0330

    datasets.py

--- vim/datasets.py
+++ vim/datasets.py
@@ -57,7 +57,7 @@ def build_dataset(is_train, args):
     transform = build_transform(is_train, args)
 
     if args.data_set == 'MNIST':
-        dataset = datasets.MNIST(args.data_path, train=is_train, transform=transform, download=True)
+        dataset = datasets.MNIST(args.data_path, train=is_train, download=True)
         nb_classes = 10
     elif args.data_set == 'IMNET':
         root = os.path.join(args.data_path, 'train' if is_train else 'val')
