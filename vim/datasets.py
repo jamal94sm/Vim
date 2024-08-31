76462e3f1514d06ce128f2595a56c2b5faed1342
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2024-08-31 19:56:33 +0330

    datasets.py

--- vim/datasets.py
+++ vim/datasets.py
@@ -57,7 +57,7 @@ def build_dataset(is_train, args):
     transform = build_transform(is_train, args)
 
     if args.data_set == 'MNIST':
-        dataset = datasets.KMNIST(args.data_path, train=is_train, transform=transform, download=True)
+        dataset = datasets.MNIST(args.data_path, train=is_train, transform=transform, download=True)
         nb_classes = 10
     elif args.data_set == 'CIFAR':
         dataset = datasets.CIFAR10(args.data_path, train=is_train, transform=transform, download=True)
