5693dc1c12f412ee50984a4fd75c077768fa5251
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2024-08-31 20:00:47 +0330

    pt-vim-t.sh

--- vim/scripts/pt-vim-t.sh
+++ vim/scripts/pt-vim-t.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python Vim/vim/main.py --batch-size 16 --drop-path 0.0 --weight-decay 0.1 
+python Vim/vim/main.py --dataset 'CIFAR' --batch-size 16 --drop-path 0.0 --weight-decay 0.1 
