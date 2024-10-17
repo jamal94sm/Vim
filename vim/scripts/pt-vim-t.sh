73d3a683c562805773b00dbc1ccd5b13657c1d8d
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2024-10-17 17:23:57 +0330

    pt-vim-t.sh

--- vim/scripts/pt-vim-t.sh
+++ vim/scripts/pt-vim-t.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #python Vim/vim/main.py --model vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 --data-set 'CIFAR' --batch-size 16 --drop-path 0.0 --weight-decay 0.1 
-python Vim/vim/main.py --model vim_tiny_patch16_stride8_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 --batch-size 128 --lr 5e-6 --min-lr 1e-5 --warmup-lr 1e-5 --drop-path 0.0 --weight-decay 1e-8 --num_workers 25 --data-path CIFAR  --epochs 30 
-
+#python Vim/vim/main.py --model vim_tiny_patch16_stride8_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2 --batch-size 128 --lr 5e-6 --min-lr 1e-5 --warmup-lr 1e-5 --drop-path 0.0 --weight-decay 1e-8 --num_workers 25 --data-path CIFAR  --epochs 30 
+python Vim/vim/main.py
