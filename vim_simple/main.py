d4abc9f539a9e240ac0a221747eeb6960a4ba4e9
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2024-10-17 15:23:53 +0330

    main.py

--- vim_simple/main.py
+++ vim_simple/main.py
@@ -32,123 +32,285 @@ import utils
 # log about
 import mlflow
 
-from torchvision import datasets, transforms
-from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('DeiT training and evaluation script', add_help=False)
+    parser.add_argument('--batch-size', default=64, type=int)
+    parser.add_argument('--epochs', default=300, type=int)
+    parser.add_argument('--bce-loss', action='store_true')
+    parser.add_argument('--unscale-lr', action='store_true')
+
+    # Model parameters
+    parser.add_argument('--model', default='deit_base_patch16_224', type=str, metavar='MODEL',
+                        help='Name of model to train')
+    parser.add_argument('--input-size', default=224, type=int, help='images input size')
+
+    parser.add_argument('--drop', type=float, default=0.0, metavar='PCT',
+                        help='Dropout rate (default: 0.)')
+    parser.add_argument('--drop-path', type=float, default=0.1, metavar='PCT',
+                        help='Drop path rate (default: 0.1)')
+
+    parser.add_argument('--model-ema', action='store_true')
+    parser.add_argument('--no-model-ema', action='store_false', dest='model_ema')
+    parser.set_defaults(model_ema=True)
+    parser.add_argument('--model-ema-decay', type=float, default=0.99996, help='')
+    parser.add_argument('--model-ema-force-cpu', action='store_true', default=False, help='')
+
+    # Optimizer parameters
+    parser.add_argument('--opt', default='adamw', type=str, metavar='OPTIMIZER',
+                        help='Optimizer (default: "adamw"')
+    parser.add_argument('--opt-eps', default=1e-8, type=float, metavar='EPSILON',
+                        help='Optimizer Epsilon (default: 1e-8)')
+    parser.add_argument('--opt-betas', default=None, type=float, nargs='+', metavar='BETA',
+                        help='Optimizer Betas (default: None, use opt default)')
+    parser.add_argument('--clip-grad', type=float, default=None, metavar='NORM',
+                        help='Clip gradient norm (default: None, no clipping)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--weight-decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
+    # Learning rate schedule parameters
+    parser.add_argument('--sched', default='cosine', type=str, metavar='SCHEDULER',
+                        help='LR scheduler (default: "cosine"')
+    parser.add_argument('--lr', type=float, default=5e-4, metavar='LR',
+                        help='learning rate (default: 5e-4)')
+    parser.add_argument('--lr-noise', type=float, nargs='+', default=None, metavar='pct, pct',
+                        help='learning rate noise on/off epoch percentages')
+    parser.add_argument('--lr-noise-pct', type=float, default=0.67, metavar='PERCENT',
+                        help='learning rate noise limit percent (default: 0.67)')
+    parser.add_argument('--lr-noise-std', type=float, default=1.0, metavar='STDDEV',
+                        help='learning rate noise std-dev (default: 1.0)')
+    parser.add_argument('--warmup-lr', type=float, default=1e-6, metavar='LR',
+                        help='warmup learning rate (default: 1e-6)')
+    parser.add_argument('--min-lr', type=float, default=1e-5, metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0 (1e-5)')
+
+    parser.add_argument('--decay-epochs', type=float, default=30, metavar='N',
+                        help='epoch interval to decay LR')
+    parser.add_argument('--warmup-epochs', type=int, default=5, metavar='N',
+                        help='epochs to warmup LR, if scheduler supports')
+    parser.add_argument('--cooldown-epochs', type=int, default=10, metavar='N',
+                        help='epochs to cooldown LR at min_lr, after cyclic schedule ends')
+    parser.add_argument('--patience-epochs', type=int, default=10, metavar='N',
+                        help='patience epochs for Plateau LR scheduler (default: 10')
+    parser.add_argument('--decay-rate', '--dr', type=float, default=0.1, metavar='RATE',
+                        help='LR decay rate (default: 0.1)')
+
+    # Augmentation parameters
+    parser.add_argument('--color-jitter', type=float, default=0.3, metavar='PCT',
+                        help='Color jitter factor (default: 0.3)')
+    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
+                        help='Use AutoAugment policy. "v0" or "original". " + \
+                             "(default: rand-m9-mstd0.5-inc1)'),
+    parser.add_argument('--smoothing', type=float, default=0.1, help='Label smoothing (default: 0.1)')
+    parser.add_argument('--train-interpolation', type=str, default='bicubic',
+                        help='Training interpolation (random, bilinear, bicubic default: "bicubic")')
+
+    parser.add_argument('--repeated-aug', action='store_true')
+    parser.add_argument('--no-repeated-aug', action='store_false', dest='repeated_aug')
+    parser.set_defaults(repeated_aug=True)
+    
+    parser.add_argument('--train-mode', action='store_true')
+    parser.add_argument('--no-train-mode', action='store_false', dest='train_mode')
+    parser.set_defaults(train_mode=True)
+    
+    parser.add_argument('--ThreeAugment', action='store_true') #3augment
+    
+    parser.add_argument('--src', action='store_true') #simple random crop
+    
+    # * Random Erase params
+    parser.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
+                        help='Random erase prob (default: 0.25)')
+    parser.add_argument('--remode', type=str, default='pixel',
+                        help='Random erase mode (default: "pixel")')
+    parser.add_argument('--recount', type=int, default=1,
+                        help='Random erase count (default: 1)')
+    parser.add_argument('--resplit', action='store_true', default=False,
+                        help='Do not random erase first (clean) augmentation split')
+
+    # * Mixup params
+    parser.add_argument('--mixup', type=float, default=0.8,
+                        help='mixup alpha, mixup enabled if > 0. (default: 0.8)')
+    parser.add_argument('--cutmix', type=float, default=1.0,
+                        help='cutmix alpha, cutmix enabled if > 0. (default: 1.0)')
+    parser.add_argument('--cutmix-minmax', type=float, nargs='+', default=None,
+                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
+    parser.add_argument('--mixup-prob', type=float, default=1.0,
+                        help='Probability of performing mixup or cutmix when either/both is enabled')
+    parser.add_argument('--mixup-switch-prob', type=float, default=0.5,
+                        help='Probability of switching to cutmix when both mixup and cutmix enabled')
+    parser.add_argument('--mixup-mode', type=str, default='batch',
+                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
+
+    # Distillation parameters
+    parser.add_argument('--teacher-model', default='regnety_160', type=str, metavar='MODEL',
+                        help='Name of teacher model to train (default: "regnety_160"')
+    parser.add_argument('--teacher-path', type=str, default='')
+    parser.add_argument('--distillation-type', default='none', choices=['none', 'soft', 'hard'], type=str, help="")
+    parser.add_argument('--distillation-alpha', default=0.5, type=float, help="")
+    parser.add_argument('--distillation-tau', default=1.0, type=float, help="")
+    
+    # * Cosub params
+    parser.add_argument('--cosub', action='store_true') 
+    
+    # * Finetuning params
+    parser.add_argument('--finetune', default='', help='finetune from checkpoint')
+    parser.add_argument('--attn-only', action='store_true') 
+    
+    # Dataset parameters
+    parser.add_argument('--data-path', default='Vim/data_set/', type=str,
+                        help='dataset path')
+    parser.add_argument('--data-set', default='CIFAR', choices=['CIFAR', 'MNIST', 'IMNET', 'INAT', 'INAT19'],
+                        type=str, help='Image Net dataset path')
+    parser.add_argument('--inat-category', default='class',
+                        choices=['kingdom', 'phylum', 'class', 'order', 'supercategory', 'family', 'genus', 'name'],
+                        type=str, help='semantic granularity')
+
+    parser.add_argument('--output_dir', default='',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
+    parser.add_argument('--eval-crop-ratio', default=0.875, type=float, help="Crop ratio for evaluation")
+    parser.add_argument('--dist-eval', action='store_true', default=False, help='Enabling distributed evaluation')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin-mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no-pin-mem', action='store_false', dest='pin_mem',
+                        help='')
+    parser.set_defaults(pin_mem=True)
+
+    # distributed training parameters
+    parser.add_argument('--distributed', action='store_true', default=False, help='Enabling distributed training')
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    
+    # amp about
+    parser.add_argument('--if_amp', action='store_true')
+    parser.add_argument('--no_amp', action='store_false', dest='if_amp')
+    parser.set_defaults(if_amp=False)
+
+    # if continue with inf
+    parser.add_argument('--if_continue_inf', action='store_true')
+    parser.add_argument('--no_continue_inf', action='store_false', dest='if_continue_inf')
+    parser.set_defaults(if_continue_inf=False)
+
+    # if use nan to num
+    parser.add_argument('--if_nan2num', action='store_true')
+    parser.add_argument('--no_nan2num', action='store_false', dest='if_nan2num')
+    parser.set_defaults(if_nan2num=False)
+
+    # if use random token position
+    parser.add_argument('--if_random_cls_token_position', action='store_true')
+    parser.add_argument('--no_random_cls_token_position', action='store_false', dest='if_random_cls_token_position')
+    parser.set_defaults(if_random_cls_token_position=False)    
+
+    # if use random token rank
+    parser.add_argument('--if_random_token_rank', action='store_true')
+    parser.add_argument('--no_random_token_rank', action='store_false', dest='if_random_token_rank')
+    parser.set_defaults(if_random_token_rank=False)
+
+    parser.add_argument('--local-rank', default=0, type=int)
+    return parser
 
 
-def main():
+def main(args):
+    #utils.init_distributed_mode(args)
+
+    print(args)
+
+
+    device = torch.device(args.device)
+
     # fix the seed for reproducibility
-    seed = 0
-    seed = seed + utils.get_rank()
+    seed = args.seed + utils.get_rank()
     torch.manual_seed(seed)
     np.random.seed(seed)
     # random.seed(seed)
-    
+
     cudnn.benchmark = True
-    
+
     # log about
-    #output_dir = 'Vim/vim_simple/output_dir/'
-    output_dir = ''
-    local_rank = 0 #?
-    run_name = output_dir.split("/")[-1]
+    run_name = args.output_dir.split("/")[-1]
     
-    batch_size = 64
-    num_workers = 10 #?
 
-###################################################################### DATA
-    ### Dataset  
-    def build_transform(input_size, eval_crop_ratio):
-        resize_im = input_size > 32
-        t = []
-        if resize_im:
-            size = int(input_size / eval_crop_ratio)
-            t.append(
-                transforms.Resize(size, interpolation=3),  # to maintain same ratio w.r.t. 224 images
-            )
-            t.append(transforms.CenterCrop(input_size))
+    dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
+    dataset_val, _ = build_dataset(is_train=False, args=args)
     
-        t.append(transforms.ToTensor())
-        t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
-        return transforms.Compose(t)
-    
-    
-    eval_crop_ratio = 0.875
-    # MNIST
-    #dataset = datasets.MNIST('Vim/data_set/', train=is_train, transform=transform, download=True)
-
-    # CIFAR
-    nb_classes = 10
-    input_size = 32
-    transform = build_transform (input_size, eval_crop_ratio)
-    dataset_train = datasets.CIFAR10('Vim/data_set/', train=True, transform=transform, download=True)
-    dataset_val = datasets.CIFAR10('Vim/data_set/', train=False, transform=transform, download=True)
-
     sampler_train = torch.utils.data.RandomSampler(dataset_train)
     sampler_val = torch.utils.data.SequentialSampler(dataset_val)
-    
-    
+
     data_loader_train = torch.utils.data.DataLoader(
         dataset_train, sampler=sampler_train,
-        batch_size=batch_size,
-        num_workers=num_workers,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
         drop_last=True,
     )
+
     data_loader_val = torch.utils.data.DataLoader(
         dataset_val, sampler=sampler_val,
-        batch_size=int(1.5 * batch_size * 20),
-        num_workers=num_workers,
+        batch_size=int(1.5 * args.batch_size * 20),
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
         drop_last=False
     )
 
-
-    ########################################################## MODEL
-    model = models_mamba.vim_tiny_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_midclstok_div2()
-    print(f"Creating model: {model}")
+    print(f"Creating model: {args.model}")
     model = create_model(
-        model,
+        args.model,
         pretrained=False,
-        num_classes=nb_classes,
-        drop_rate=0.0,
-        drop_path_rate=0.1,
+        num_classes=args.nb_classes,
+        drop_rate=args.drop,
+        drop_path_rate=args.drop_path,
         drop_block_rate=None,
-        img_size=input_size
+        img_size=args.input_size
     )
-    
+            
+    model.to(device)
+
+    model_without_ddp = model
+
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('number of params:', n_parameters)
 
-    args = SimpleNamespace()
-    args.sched = 'cosine'
-    args.clip_grad = None
-    args.weight_decay = 0
-    args.lr = 1e-4
-    args.opt = 'adam' 
-    args.momentum = 0.9
+    if not args.unscale_lr:
+        linear_scaled_lr = args.lr * args.batch_size * utils.get_world_size() / 512.0
+        args.lr = linear_scaled_lr
+    optimizer = create_optimizer(args, model_without_ddp)
+    
+    # amp about
+    amp_autocast = suppress
+    loss_scaler = "none"
 
-    optimizer = create_optimizer(args, model)
     lr_scheduler, _ = create_scheduler(args, optimizer)
 
     criterion = torch.nn.CrossEntropyLoss()
-
-    output_dir = Path(output_dir)
+    #criterion = SoftTargetCrossEntropy()
+    #criterion = LabelSmoothingCrossEntropy(smoothing=args.smoothing)
         
+    output_dir = Path(args.output_dir)
+    
     print(f"Start training for {args.epochs} epochs")
     start_time = time.time()
     max_accuracy = 0.0
-    epochs = 20
-    amp_autocast = suppress
-    loss_scaler = "none"
-    for epoch in range(0, epochs):
+    for epoch in range(args.start_epoch, args.epochs):
 
         train_stats = train_one_epoch(
             model, criterion, data_loader_train,
             optimizer, device, epoch, loss_scaler, amp_autocast,
-            args.clip_grad,
-            args=args
+            args.clip_grad, model_ema, mixup_fn,
+            set_training_mode=args.train_mode,  # keep in eval mode for deit finetuning / train mode for training and deit III finetuning
+            args=args,
         )
 
         lr_scheduler.step(epoch)
-        if output_dir:
+        if args.output_dir:
             checkpoint_paths = [output_dir / 'checkpoint.pth']
             for checkpoint_path in checkpoint_paths:
                 utils.save_on_master({
@@ -167,7 +329,7 @@ def main():
         
         if max_accuracy < test_stats["acc1"]:
             max_accuracy = test_stats["acc1"]
-            if output_dir:
+            if args.output_dir:
                 checkpoint_paths = [output_dir / 'best_checkpoint.pth']
                 for checkpoint_path in checkpoint_paths:
                     utils.save_on_master({
@@ -181,10 +343,12 @@ def main():
                     }, checkpoint_path)
             
         print(f'Max accuracy: {max_accuracy:.2f}%')
+
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                     **{f'test_{k}': v for k, v in test_stats.items()},
+                     'epoch': epoch,
+                     'n_parameters': n_parameters}
         
-        if output_dir and utils.is_main_process():
-            with (output_dir / "log.txt").open("a") as f:
-                f.write(json.dumps(log_stats) + "\n")
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
@@ -192,7 +356,8 @@ def main():
 
 
 if __name__ == '__main__':
-    output_dir = False
-    if output_dir:
-        Path(output_dir).mkdir(parents=True, exist_ok=True)
-    main()
+    parser = argparse.ArgumentParser('DeiT training and evaluation script', parents=[get_args_parser()])
+    args = parser.parse_args()
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    main(args)
