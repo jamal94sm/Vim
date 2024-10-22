70ca0bafd1f53aaf7bf6ac10efadc5117a59eb57
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2024-10-22 21:34:57 +0330

    Update mamba.sh

--- mamba.sh
+++ mamba.sh
@@ -11,4 +11,4 @@ module purge
 module load python
 source ~/Mamba/bin/activate
 
-python Vim/vim/main.py --batch-size 8 --drop-path 0.0 --weight-decay 0.1 
+python Vim/pytorch_visionmamba/pytorch_visionmamba.py 
